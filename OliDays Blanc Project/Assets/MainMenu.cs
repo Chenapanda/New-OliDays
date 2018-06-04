52561c7ba3e573b9ae969db72f44e190c0f1adc8
doudou80090 <mngeneralbrun@gmail.com>
2018-06-04 16:31:17 +0200

    Menu options
    Je crée les options

---
+++ OliDays Blanc Project/Assets/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+public class MainMenu : MonoBehaviour {
+
+	public void Jouer()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void Quitter()
+	{
+		Debug.Log("Quitter");
+		Application.Quit();
+	}
+}
