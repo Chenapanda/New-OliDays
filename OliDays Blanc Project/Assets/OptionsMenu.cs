89a47dde3ed3d6fca2b440e7b7b86e25c50bb8a4
doudou80090 <mngeneralbrun@gmail.com>
2018-06-04 19:22:01 +0200

    OptionsMenu
    on a le script pour le son

--- OliDays Blanc Project/Assets/OptionsMenu.cs
+++ OliDays Blanc Project/Assets/OptionsMenu.cs
@@ -3,23 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using System.IO;
 
 public class OptionsMenu : MonoBehaviour
 {
 
-	public AudioMixer volumeMixer;
 	
-	Resolution[] resolutions;
-
-	public Dropdown resolutionsDropdown;
+	public Dropdown textureQualityDropdown;
+	public Slider musicVolumeSlider;
 	
-	public void Volume(float volume)
-	{
-		volumeMixer.SetFloat("volume", volume);
-	}
+	
+	public GameSettings gameSettings;
+	public AudioSource musicSource;
 
 	public void FullScreen(bool isFullScreen)
 	{
 		Screen.fullScreen = isFullScreen;
 	}
+
+	void OnEnable()
+	{
+		gameSettings = new GameSettings();
+		textureQualityDropdown.onValueChanged.AddListener(delegate { OnTexttureQualityChange(); });
+		musicVolumeSlider.onValueChanged.AddListener(delegate { OnMusicVolumeChange(); });
+	
+	}
+
+	public void OnMusicVolumeChange()
+	{
+		musicSource.volume = gameSettings.musicVolume = musicVolumeSlider.value;
+	}
+	
+	public void OnTexttureQualityChange()
+	{
+		QualitySettings.masterTextureLimit = gameSettings.textureQuality = textureQualityDropdown.value;
+	}
+
 }
