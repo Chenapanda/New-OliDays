fe9b3a7c1405a39840b3dd3a8939182ded7ef939
doudou80090 <mngeneralbrun@gmail.com>
2018-06-05 14:13:22 +0200

    Retour sur les menus
    Je vais refaire le truc de l'anglais c'est nul en fait

--- OliDays Blanc Project/Assets/Scripts/MainMenu.cs
+++ OliDays Blanc Project/Assets/Scripts/MainMenu.cs
@@ -1,18 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 
-public class MainMenu : MonoBehaviour {
-	
-	
+public class MainMenu : MonoBehaviour
+{
+
 	public void Jouer()
 	{
 		SceneManager.LoadScene("Main", LoadSceneMode.Single);
 	}
-
 	public void Quitter()
 	{
 		Debug.Log("Quitter");
