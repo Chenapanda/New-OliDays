dbc08fa0428e5a9a96373dffe85a6f249fe468ef
doudou80090 <mngeneralbrun@gmail.com>
2018-06-06 16:19:22 +0200

    EnglishVersion
    J'ai fini on a tout en anglais maintenant

---
+++ OliDays Blanc Project/Assets/LanguageChange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LanguageChange : MonoBehaviour
+{
+    //Make sure to attach these Buttons in the Inspector
+    public Button langue;
+
+    void Start()
+    {
+        Button btn = langue.GetComponent<Button>();
+
+        //Calls the TaskOnClick method when you click the Button
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        //Output this to console when the Button is clicked
+        SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+    }
+}
---
+++ OliDays Blanc Project/Assets/Scripts/MainMenuAnglais.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class MainMenuAnglais : MonoBehaviour
+{
+
+	public void Jouer()
+	{
+		SceneManager.LoadScene("MainAnglais", LoadSceneMode.Single);
+	}
+	public void Quitter()
+	{
+		Debug.Log("Quitter");
+		Application.Quit();
+	}
+
+}
+
