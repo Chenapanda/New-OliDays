669dbcad2add271ad33e2dd68ad4cd31f6007f67
Chenapanda <hugo.santos@epita.fr>
2018-06-06 16:29:15 +0200

    Revert "Revert "Merge branch 'master' of https://github.com/Chenapanda/New-OliDays""
    This reverts commit c4753b014062cf83f42e006276a42e210c161acb.

--- OliDays Blanc Project/Assets/Scripts/PowerUp.cs
+++ OliDays Blanc Project/Assets/Scripts/PowerUp.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PowerUp : MonoBehaviour {
+    public int type; //1 = dammage 2 = hp 3 = attackspeed
     public int bonus;
     public GameObject pickupEffect;
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            other.GetComponent<PlayerMovement>().Dammage += bonus;
+            if (type == 1)
+            {
+                other.GetComponent<PlayerMovement>().Dammage += bonus;
+            }
+            if (type == 2)
+            {
+                other.GetComponent<PlayerMovement>().Health += bonus;
+            }
             Instantiate(pickupEffect, transform.position + new Vector3(0, .5f, 0), pickupEffect.transform.rotation);
             Destroy(gameObject);
 
