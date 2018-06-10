90023759f1eab047bba1b37426c1a8a0ad08a02d
Chenapanda <hugo.santos@epita.fr>
2018-06-10 22:14:33 +0200

    Shop V.1

---
+++ OliDays Blanc Project/Assets/ShopItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopItem : PowerUp {
+    public PowerUp powerup;
+    public int cost;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (cost <= other.GetComponent<PlayerMovement>().Dreams)
+            {
+                Power(other);
+                other.GetComponent<PlayerMovement>().Dreams -= cost;
+            }
+        }
+
+    }
+}
