a3d776df9589992e53009a26b1485e96c7498c56
doudou80090 <mngeneralbrun@gmail.com>
2018-06-12 21:30:24 +0200

    sounds

---
+++ OliDays Blanc Project/Assets/BackgroundSound.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundSound : MonoBehaviour
+{
+
+	public Slider MenuSlider;
+	public AudioSource AudioSource;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+
+
+	private static BackgroundSound instance = null;
+
+	public static BackgroundSound Instance
+	{
+		get { return instance; }
+	}
+
+	private void Awake()
+	{
+		if (instance != null && instance != this)
+		{
+			Destroy(this.gameObject);
+			return;
+		}
+		else
+		{
+			instance = this;
+		}
+
+		DontDestroyOnLoad(this.gameObject);
+		AudioSource.volume = MenuSlider.value;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		AudioSource.volume = MenuSlider.value;
+	}
+}
